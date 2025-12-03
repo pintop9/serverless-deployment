3abaa4e1edfbe53e496df5dc0ef7a28039a099f9
pintop9 <130343408+pintop9@users.noreply.github.com>
2025-12-03 21:02:36 +0200

    Add CORS headers to response

--- view_groups/app.py
+++ view_groups/app.py
@@ -23,6 +23,11 @@ def lambda_handler(event, context):
 
     return {
         "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "*", 
+            "Access-Control-Allow-Methods": "GET"
+        },
         "body": json.dumps(groups)
     }
 
